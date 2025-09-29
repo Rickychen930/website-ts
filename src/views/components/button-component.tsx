3605d4e580561e73da3c5dcc269a12b54667c18f
blackver69 <46539728+blackver69@users.noreply.github.com>
2025-09-29 22:01:55 +1000

    init: create button component

---
+++ src/views/components/button-component.tsx
@@ -0,0 +1,61 @@
+import React, { Component, ReactNode } from "react";
+import { ButtonType, ButtonVariant, ComponentState } from "../../types/ui";
+
+type ButtonProps = {
+  children: ReactNode;
+  onClick?: () => void;
+  type?: ButtonType;
+  variant?: ButtonVariant;
+  disabled?: ComponentState;
+};
+
+class Button extends Component<ButtonProps> {
+  static defaultProps = {
+    type: ButtonType.BUTTON,
+    variant: ButtonVariant.PRIMARY,
+    disabled: ComponentState.ACTIVE,
+  };
+
+  private _component: ReactNode;
+
+  constructor(props: ButtonProps) {
+    super(props);
+    this._component = this._createComponent();
+  }
+
+  protected _createComponent(): ReactNode {
+    const { children, onClick, type, variant, disabled } = this.props;
+
+    const isDisabled =
+      disabled === ComponentState.DISABLED ||
+      disabled === ComponentState.LOADING;
+
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        disabled={isDisabled}
+        className={`btn btn-${variant}`}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  public componentDidUpdate(prevProps: ButtonProps) {
+    if (
+      prevProps.children !== this.props.children ||
+      prevProps.variant !== this.props.variant ||
+      prevProps.disabled !== this.props.disabled ||
+      prevProps.type !== this.props.type
+    ) {
+      this._component = this._createComponent();
+    }
+  }
+
+  public render() {
+    return this._component;
+  }
+}
+
+export default Button;
